ac76b37a3179b6ee06a437495601b63f8a5de571
Georgi Krystev <gkrystev@lirex.com>
2017-09-20 17:17:54 +0300

    first commit

---
+++ src/webparts/spFxOnPrenPnPjs/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpFxOnPrenPnPjsStrings {
+  PropertyPaneDescription: string;
+  DataGroupName: string;
+  ListNameFieldLabel: string;
+}
+
+declare module 'spFxOnPrenPnPjsStrings' {
+  const strings: ISpFxOnPrenPnPjsStrings;
+  export = strings;
+}
